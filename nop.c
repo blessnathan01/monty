5df3516193f577c409d38b8478493f948ceee6e9
Victoria-20 <victoriaocon111@gmail.com>
2022-06-16 18:59:07 +0300

    nop opcode

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+
+/**
+ * _nop - function does nothing
+ * @pile: pointer
+ * @line_number: line number
+ * Return: void
+ */
+
+void  _nop(stack_t **stack, unsigned  int line_number)
+{
+	UNUSED(stack);
+	UNUSED(line_number);
+}
