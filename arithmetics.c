29d2fe02f4cecfcd21c341dc46f532c659c42d52
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-19 15:01:47 -0700

    Update arithmetics.c

--- arithmetics.c
+++ arithmetics.c
@@ -1,129 +1,132 @@
 #include "monty.h"
 
 /**
- * add - Add the top two values of the stack, store sum in second position,
- * set *stack to second node, and remove top node
- * @stack: Dopule pointer to the top of the stack
- * @line_num: The line the command was found in the monty file
- **/
+ * add - adds the top two elements of the stack and stores sum in second node,
+ * set the pointer to the second node, then remove the top node
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void add(stack_t **stack, unsigned int line_num)
 {
-	stack_t *walker;
+	stack_t *tmp;
 
-	walker = *stack;
-	if (walker == NULL || walker->next == NULL)
+	tmp = *stack;
+	if (tmp == NULL || tmp->next == NULL)
 	{
 		printf("L%u: can't add, stack too short\n", line_num);
 		ret_and_q.opcode_return = 1;
 	}
 	if (ret_and_q.opcode_return != 1)
 	{
-		walker->next->n += walker->n;
+		tmp->next->n += tmp->n;
 		pop(stack, line_num);
 	}
 }
 
 /**
- * sub - Subtract the top element of the stack from the second element,
- * store result in second position,set *stack to second node,
- * and remove top node
- * @stack: Dopule pointer to the top of the stack
- * @line_num: The line the command was found in the monty file
- **/
+ * sub - subtracts the top element of the stack from the second
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void sub(stack_t **stack, unsigned int line_num)
 {
-	stack_t *walker;
+	stack_t *tmp;
 
-	walker = *stack;
-	if (walker == NULL ||  walker->next == NULL)
+	tmp = *stack;
+	if (tmp == NULL ||  tmp->next == NULL)
 	{
 		printf("L%u: can't sub, stack too short\n", line_num);
 		ret_and_q.opcode_return = 1;
 	}
 	if (ret_and_q.opcode_return != 1)
 	{
-		walker->next->n -= walker->n;
+		tmp->next->n -= tmp->n;
 		pop(stack, line_num);
 	}
 }
 
 /**
- * _div - Divide the second element of the stack by the first element of the
- * stack, store result in second position, set *stack to second node,
- * and remove top node
- * @stack: Double pointer to the top of the stack
- * @line_num: The line the command was found in the monty file
- **/
+ * _div - divides the second element of the stack by the first
+ * @stack: double pointer of the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void _div(stack_t **stack, unsigned int line_num)
 {
-	stack_t *walker;
+	stack_t *tmp;
 
-	walker = *stack;
-	if (walker == NULL || walker->next == NULL)
+	tmp = *stack;
+	if (tmp == NULL || tmp->next == NULL)
 	{
 		printf("L%u: can't div, stack too short\n", line_num);
 		ret_and_q.opcode_return = 1;
 	}
-	if (walker != NULL && ret_and_q.opcode_return != 1 && walker->n == 0)
+	if (tmp != NULL && ret_and_q.opcode_return != 1 && tmp->n == 0)
 	{
 		printf("L%u: division by zero\n", line_num);
 		ret_and_q.opcode_return = 1;
 	}
 	if (ret_and_q.opcode_return != 1)
 	{
-		walker->next->n /= walker->n;
+		tmp->next->n /= tmp->n;
 		pop(stack, line_num);
 	}
 }
 
 /**
- * mul - Multiply the top two values of the stack, store result in second
- * position, set *stack to second node, and remove top node
- * @stack: Double pointer to the top of the stack
- * @line_num: The line the command was found in the monty file
- **/
+ * mul - multiplies the top two elements of the stack
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void mul(stack_t **stack, unsigned int line_num)
 {
-	stack_t *walker;
+	stack_t *tmp;
 
-	walker = *stack;
-	if (walker == NULL || walker->next == NULL)
+	tmp = *stack;
+	if (tmp == NULL || tmp->next == NULL)
 	{
 		printf("L%u: can't mul, stack too short\n", line_num);
 		ret_and_q.opcode_return = 1;
 	}
 	if (ret_and_q.opcode_return != 1)
 	{
-		walker->next->n *= walker->n;
+		tmp->next->n *= tmp->n;
 		pop(stack, line_num);
 	}
 }
 
 /**
- * mod - Mod the second element of the stack by the first element of the stack,
- * store the result in second position, set *stack to second node,
- * and remove top node
- * @stack: Double pointer to the top of the stack
- * @line_num: The line the command was found in the monty file
- **/
+ * mod - calculates the remainder after division of two top elements of the stack
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void mod(stack_t **stack, unsigned int line_num)
 {
-	stack_t *walker;
+	stack_t *tmp;
 
-	walker = *stack;
-	if (walker == NULL || walker->next == NULL)
+	tmp = *stack;
+	if (tmp == NULL || tmp->next == NULL)
 	{
 		printf("L%u: can't mod, stack too short\n", line_num);
 		ret_and_q.opcode_return = 1;
 	}
-	if (walker != NULL && walker->n == 0 && ret_and_q.opcode_return != 1)
+	if (tmp != NULL && tmp->n == 0 && ret_and_q.opcode_return != 1)
 	{
 		printf("L%u: division by zero\n", line_num);
 		ret_and_q.opcode_return = 1;
 	}
 	if (ret_and_q.opcode_return != 1)
 	{
-		walker->next->n %= walker->n;
+		tmp->next->n %= tmp->n;
 		pop(stack, line_num);
 	}
 }
