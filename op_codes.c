531f8dfb9481a097cf4f10debf32b7acf4932553
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-19 16:19:23 -0700

    Update op_codes.c

--- op_codes.c
+++ op_codes.c
@@ -1,78 +1,85 @@
 #include "monty.h"
 
 /**
- * pall - Print all values in the stack
- * @stack: Double pointer to the top of the stack
- * @line_num: The line of the file the command was found
- **/
+ * pall - prints all elements in the stack
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void pall(stack_t **stack, unsigned int line_num)
 {
-	stack_t *walker;
+	stack_t *tmp;
 
 	(void) line_num;
-	walker = *stack;
-	while (walker != NULL)
+	tmp = *stack;
+	while (tmp != NULL)
 	{
-		printf("%d\n", walker->n);
-		walker = walker->next;
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
 	}
 }
 
 /**
- * pop - Remove the top element of the stack
- * @stack: Double pointer to the top of the stack
- * @line_num: The line of the file the command was found
- **/
+ * pop - removes the top element of the stack
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void pop(stack_t **stack, unsigned int line_num)
 {
-	stack_t *kill_node, *walker;
+	stack_t *kill_node, *tmp;
 
-	walker = *stack;
-	if (walker == NULL)
+	tmp = *stack;
+	if (tmp == NULL)
 	{
 		printf("L%u: can't pop an empty stack\n", line_num);
 		ret_and_q.opcode_return = 1;
 	}
 	if (ret_and_q.opcode_return != 1)
 	{
-		kill_node = walker;
-		*stack = walker->next;
-		if (walker != NULL)
-			walker->prev = NULL;
+		kill_node = tmp;
+		*stack = tmp->next;
+		if (tmp != NULL)
+			tmp->prev = NULL;
 		free(kill_node);
 	}
 }
 
 /**
- * swap - Put the second element on top of the stack, put the top element
- * in the second position
- * @stack: Double pointer to the top of the stack
- * @line_num: The line of the file the command was found
- **/
+ * swap - swaps two top elements of the stack
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void swap(stack_t **stack, unsigned int line_num)
 {
-	stack_t *walker;
+	stack_t *slider;
 	int tmp;
 
-	walker = *stack;
-	if (walker == NULL || walker->next == NULL)
+	slider = *stack;
+	if (slider == NULL || slider->next == NULL)
 	{
 		printf("L%u: can't swap, stack too short\n", line_num);
 		ret_and_q.opcode_return = 1;
 	}
 	if (ret_and_q.opcode_return != 1)
 	{
-		tmp = walker->n;
-		walker->n = walker->next->n;
-		walker->next->n = tmp;
+		tmp = slider->n;
+		slider->n = slider->next->n;
+		slider->next->n = tmp;
 	}
 }
 
 /**
- * pint - Print the top element of the stack
- * @stack: Double pointer to the top of the stack
- * @line_num: The line of the file the command was found
- **/
+ * pint - prints the top element of the stack
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void pint(stack_t **stack, unsigned int line_num)
 {
 	if (*stack == NULL)
