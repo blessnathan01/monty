347af209e5a7a719aaf7d6508af9c7978b4b6923
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-19 16:09:20 -0700

    Update more_opcodes.c

--- more_opcodes.c
+++ more_opcodes.c
@@ -1,11 +1,13 @@
 #include "monty.h"
 
 /**
- * pchar - Print out the element of the code as it's corresponding value
+ * pchar - prints out the element of the code as it's corresponding value
  * on the ASCII table
- * @stack: Double pointer to the top of the stack
- * @line_num: The line that the command was found in the file
- **/
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void pchar(stack_t **stack, unsigned int line_num)
 {
 	if (*stack != NULL)
@@ -29,24 +31,26 @@ void pchar(stack_t **stack, unsigned int line_num)
 }
 
 /**
- * pstr - Print out every element of the stack as the corresponding character
+ * pstr - prints out every element of the stack as the corresponding character
  * on the ASCII table, stops if the value is 0, the stack is over, or the
  * value of the element is not in the ASCII table
- * @stack: Double pointer to the top of the stack
- * @line_num: The line that the command was found in the file
- **/
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void pstr(stack_t **stack, unsigned int line_num)
 {
-	stack_t *walker;
+	stack_t *tmp;
 
 	(void) line_num;
 	if (*stack != NULL)
 	{
-		walker = *stack;
-		while (walker != NULL && walker->n > 0 && walker->n <= 127)
+		tmp = *stack;
+		while (tmp != NULL && tmp->n > 0 && tmp->n <= 127)
 		{
-			putchar(walker->n);
-			walker = walker->next;
+			putchar(tmp->n);
+			tmp = tmp->next;
 		}
 	}
 	putchar('\n');
