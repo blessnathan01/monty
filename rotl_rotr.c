132332cdf288a910d6a2ca11012afa5e6ad972e5
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-19 16:26:59 -0700

    Update rotl_rotr.c

--- rotl_rotr.c
+++ rotl_rotr.c
@@ -1,26 +1,27 @@
 #include "monty.h"
 
 /**
- * rotl - Take the top element of the stack and put it on the bottom,
- * the second element of the stack becomes the top
- * @stack: Double pointer to the top of the stack
- * @line_num: The line that the command was found
- **/
+ * rotl - puts top element at the bottom and second element at the top of stack
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void rotl(stack_t **stack, unsigned int line_num)
 {
-	stack_t *walker, *first_node, *second_node;
+	stack_t *slider, *first_node, *second_node;
 
 	(void) line_num;
 	if (*stack != NULL && (*stack)->next != NULL)
 	{
-		walker = first_node = *stack;
-		second_node = walker->next;
-		while (walker->next != NULL)
+		slider = first_node = *stack;
+		second_node = slider->next;
+		while (slider->next != NULL)
 		{
-			walker = walker->next;
+			slider = slider->next;
 		}
-		walker->next = first_node;
-		first_node->prev = walker;
+		slider->next = first_node;
+		first_node->prev = slider;
 		first_node->next = NULL;
 		second_node->prev = NULL;
 		*stack = second_node;
@@ -28,10 +29,12 @@ void rotl(stack_t **stack, unsigned int line_num)
 }
 
 /**
- * rotr - The last element of the stack becomes the top element of the stack
- * @stack: Double pointer to the top of the stack
- * @line_num: The line that the command was found
- **/
+ * rotr - puts the bottom element at the top of stack
+ * @stack: double pointer to the stack
+ * @line_num: line number
+ *
+ * Return: nothing.
+ */
 void rotr(stack_t **stack, unsigned int line_num)
 {
 	stack_t *last_node, *second_last_node;
