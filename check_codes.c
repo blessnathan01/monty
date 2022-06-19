ef9d9737b3106b5a235d9b3cb093975dbae989b6
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-19 15:30:33 -0700

    Update check_codes.c

--- check_codes.c
+++ check_codes.c
@@ -3,13 +3,14 @@
 opret_q ret_and_q = {0, 0};
 
 /**
- * check_codes - Check the found command against all possible opcodes
- * @command: The command found on the line
- * @stack: Double pointer to the top of the stack
- * @line_num: The line that the command was found
- * Return: 1 if no command is found, return opcode_return if command was
- * found. (opcode_return is changed by the opcodes, if they fail)
- **/
+ * check_codes - checks a command against all possible opcodes
+ * @command: command found on a line
+ * @stack: double pointer to the stack
+ * @line_num: the line that the command was found
+ *
+ * Return: 1 if no command found,
+ * opcode_return if command was found.
+ */
 int check_codes(char *command, stack_t **stack, size_t line_num)
 {
 	instruction_t opcodes[] = {
@@ -28,17 +29,17 @@ int check_codes(char *command, stack_t **stack, size_t line_num)
 		{"mod", mod},
 		{NULL, NULL}
 	};
-	size_t i;
+	size_t x;
 
-	i = 0;
-	while (opcodes[i].opcode != NULL)
+	x = 0;
+	while (opcodes[x].opcode != NULL)
 	{
-		if (strcmp(command, opcodes[i].opcode) == 0)
+		if (strcmp(command, opcodes[x].opcode) == 0)
 		{
-			opcodes[i].f(stack, line_num);
+			opcodes[x].f(stack, line_num);
 			return (ret_and_q.opcode_return);
 		}
-		i++;
+		x++;
 	}
 	printf("L%lu: unknown instruction %s\n", line_num, command);
 	ret_and_q.opcode_return = 1;
